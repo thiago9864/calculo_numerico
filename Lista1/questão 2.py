3c99cf04b225118808a343e5a2aded0cdd6f1ba9
renanNun <42500047+renanNun@users.noreply.github.com>
2018-08-26 13:50:44 -0300

    Update questão 2.py

--- Lista1/questão 2.py
+++ Lista1/questão 2.py
@@ -15,9 +15,15 @@ c1 = - 1 - c2
 
 c2 = (exp((-1)/sqtr(E)) - 1) / (exp(1/sqtr(E)) - exp((-1)/sqtr(E))) 
 
+valoresIniciais = 0 # u(0) = u(1) = 0
+
 #Definindo as funções
 def solucao_exata(): #solução exata dada
     return c1 * exp( ((-1) * x) / sqtr(E)) +   c2 * exp( x / sqtr(E)) + 1
 
 def solucao_original(u): #derivada segunda original
     return (1 - u) / ((-1) * E)
+
+#Solicitar o valor de E
+#testes com E = 0.1, 0.01, 0.001, 0.0001
+E = 0.1
