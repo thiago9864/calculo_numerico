1ff3b4c016eac838ea0550c10e02b48ed3061359
renanNun <42500047+renanNun@users.noreply.github.com>
2018-08-27 11:32:57 -0300

    Update questão 2.py

--- Lista1/questão 2.py
+++ Lista1/questão 2.py
@@ -35,38 +35,4 @@ def F(x):
 E = 0.1
 
 
-#analitica
-#Expansão com formula de Taylor até a 2 ordem
 
-xx = np.linspace (0, 1)
-yy = np.zeros_like(xx) #retorna uma matriz de zeros
-for i,x in enumerate(xx):
-    yy[i] = F(x)
-    
-
-#Definição numerica
-def un(N):
-#malha
-   a = 0
-   b = 1
-   h = (b-a)/(N-1)
-   xp = np.linspace(a,b,N) #começa em 0 e termina em 1
-
-   A = np.zeros((N,N)) #retorna uma matriz de zeros
-   b = np.zeros(N) #retorna uma matriz de zeros
-
-   A[0,0] = 1
-   b[0] = 0
-   for i in np.arange(1,N-1):
-       A[i,i-1] = 1
-       A[i,i] = -2
-       A[i,i+1] = 1
-       b[i] = #Formula com o polinomio de Taylor
-   A[N-1,N-1] = 1
-   b[N-1] = 0
-
-   u = np.linalg.solve(A,b)
-   return (xp,u)
-
-xx, F = un(11)
-ax.plot(xx, F, 'ro', markersize=4)
