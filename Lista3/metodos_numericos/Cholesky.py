ae8cbd3035daf9b65c35d4b41bc404ed242d555d
renanNun <42500047+renanNun@users.noreply.github.com>
2018-10-14 09:32:19 -0300

    Sugestão
    É só uma ideia pro Cho, porque tá igual inclusive as exceções, vou pesquisar mais... só que por enquanto vou deixar isso aqui comentado

--- Lista3/metodos_numericos/Cholesky.py
+++ Lista3/metodos_numericos/Cholesky.py
@@ -57,3 +57,17 @@ class Cholesky():
         #Gtx=y
         retrosub2 = RetroSubstituicao().executar(Gt, retrosub1[0])
         return [retrosub2[0], retrosub2[1] + passos]
+    
+    ''' 
+    Renan: Eu encontrei esse código aqui num arquivo da internet e fui comparando com o seu, tá igual, mas por via das dúvidas vou deixar implementado ele aqui em baixo
+    #https://rosettacode.org/wiki/Cholesky_decomposition
+    def cholesky(A):
+    L = [[0.0] * len(M) for _ in range(len(M))]
+    for i in range(len(M)):
+        for j in range(i+1):
+            s = soma(L[i][k] * L[j][k] for k in range(j))
+            if (i == j):
+                L[i][j] = sqrt(M[i][i] - s) 
+            else:1
+                L[i][j] = (1.0 / L[j][j] * (M[i][j] - s))
+'''
