32be98a172081674f1ba54b477ba6384416d8cd7
renanNun <42500047+renanNun@users.noreply.github.com>
2018-10-24 22:24:39 -0300

    Metodos não testados

--- Lista4/interpoladores/Lagrange.py
+++ Lista4/interpoladores/Lagrange.py
@@ -6,7 +6,18 @@ Created on Sat Oct 20 21:05:05 2018
 @author: Thiago Almeida
 """
 
-class Lagrange():
+class Lagrange(n,x,y,z):
 
-    def executar(self):
-        return 0
+    def executar(self,n,x,y,z):
+        
+        r = 0
+        for i in range (1,n):
+            c = 1
+            d = 1
+            for j in range (1,n):
+                if (i != j):
+                    c = c * (z - x[j])
+                    d = d * (x[i] - x[j])
+            r = r + y[i] * (c/d)
+        
+        return r
--- Lista4/interpoladores/Newton.py
+++ Lista4/interpoladores/Newton.py
@@ -6,7 +6,11 @@ Created on Sat Oct 20 21:09:49 2018
 @author: Thiago Almeida
 """
 
-class Newton():
+class Newton(n,x,y):
 
-    def executar(self):
-        return 0
+    def executar(self,n,x,y):
+        for i in range(n):
+            d[i] = y[i]
+        for k in range (1,n):
+            d[i] = (d[i] - d[i-1])/(x[i] - x[i-k])
+        return d
