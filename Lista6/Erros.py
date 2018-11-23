ff394f9bd89e086efd165fc31b27aaa4b281b02d
renanNun <42500047+renanNun@users.noreply.github.com>
2018-11-23 17:36:57 -0200

    Classe de Erros adicionada

--- Lista6/Erros.py
+++ Lista6/Erros.py
@@ -13,7 +13,7 @@ class Erro():
     def erroDaNorma(self,num_part):
       erroL2 = 0    
       
-      for i in range(0:num_part-1):
+      for i in range(0,num_part-1):
           erroL2 = erroL2 + NewtonCotes().Simpsom38(,+1,)
       
         erroL2 = np.sqrt(erroL2)
