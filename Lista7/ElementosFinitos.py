174043a700874dee4fdc2efdf4e165d7550a77a6
Thiago <thiago9863@gmail.com>
2018-12-03 11:54:20 -0200

    Lista7 - funções de base

--- Lista7/ElementosFinitos.py
+++ Lista7/ElementosFinitos.py
@@ -16,6 +16,50 @@ class ElementosFinitos():
     Fazer uma tabela de funcoes base e derivadas delas
     olhar -> https://jayemmcee.wordpress.com/lagrange-polynomial-interpolation/
     '''
-    
-    def funcaoBase(self):
         
+    
+    def funcaoBase(self, n, x, xk):
+        
+        L = np.zeros((n,), dtype=np.float128)
+        dL = np.zeros((n,), dtype=np.float128)
+        
+        #funcao de base
+        for i in range(n):
+            c = 1.0
+            d = 1.0
+            for j in range(n):
+                if(i != j):
+                    c = c * (xk - x[j])
+                    d = d * (x[i] - x[j])
+            L[i] = c / d
+            
+        #derivada da funcao de base
+        for i in range(n):
+            c = 0
+            d = 1.0
+            print("i=" + str(i))
+            
+            for j in range(n):
+                if(i != j):  
+                    d = d * (x[i] - x[j])
+            
+            #calcula as derivadas pro regra do produto  
+            for j in range(n):
+                if(i != j):
+                    print("j=" + str(j))
+                    #calcula regra do produto
+                    r = 1.0
+                    for d in range(n):
+                       if(d != j and d != i):
+                           print("d=" + str(d))
+                           r = r * (xk - x[d])
+                       #soma regra do produto
+                       c += r
+                   
+            print("-")             
+            dL[i] = c / d
+            
+        return (L, dL)
+                    
+            
+            
--- Lista7/questao1.py
+++ Lista7/questao1.py
@@ -17,11 +17,16 @@ from ElementosFinitos import ElementosFinitos
 
 
 #define o tamanho dos graficos
-figure(num=None, figsize=(8, 6), dpi=72, facecolor='w', edgecolor='k')
+#figure(num=None, figsize=(8, 6), dpi=72, facecolor='w', edgecolor='k')
 
 
 ###### Testes #######
 
 #funcao do slide
 
-ElementosFinitos().funcaoBase()
+x = [-1.0, 0, 1.0]
+xk = -1.0
+(L, dL) = ElementosFinitos().funcaoBase(3, x, xk)
+
+print(L)
+print(dL)
