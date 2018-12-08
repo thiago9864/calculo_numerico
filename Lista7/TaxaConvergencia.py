236cf62c4a00eb6d82f8b80e577602843d8d6ce5
renanNun <42500047+renanNun@users.noreply.github.com>
2018-12-08 21:23:08 -0200

    em progresso
    Tô ajeitando os coeficientes e a função mas pra você ter uma ideia do que eu to fazendo

--- Lista7/TaxaConvergencia.py
+++ Lista7/TaxaConvergencia.py
@@ -99,6 +99,23 @@ for i in range(1, 4):#elementos
         
         erro = 0
         
+def erroDaNormaL2(self,num_elem,K,num_nel,f,w,det):
+    #K = numero de pontos de integração do Gauss
+    #w(l) = pesos de integração de Gauss
+    #num_nels = numeros de nos do elemento
+    #det(l) = h/2
+    for n in range(num_elem):
+        aux_err = 0
+         for l in range(K):
+             uh = 0
+             xx = 0
+             for i in range(num_nel):
+                 uh = uh + xx + shg(l,i,l) * u(i)
+                 xx = xx + shg(l,i,l) * xl(i)
+            aux_err = aux_err + ((f(xx)- uh)**2) * w(l) * det(l)
+        erro = erro + aux_err
+    erro = sqrt(erro)
+        
         ############################################
         
         n[j-1][i-1] = np.log(erro)
